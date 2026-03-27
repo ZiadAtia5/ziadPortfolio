227d23bf6c9263a6fe098c85222e44dbf756670e
ZiadAtia5 <zezoatia10@gmail.com>
2026-03-27 02:01:12 +0200

    Fix: replace swc with standard react plugin and set base path

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/react-swc";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-
-    base:
-      command === "build" && process.env.NODE_ENV === "production" ? "/" : "/",
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: "/",
 });
